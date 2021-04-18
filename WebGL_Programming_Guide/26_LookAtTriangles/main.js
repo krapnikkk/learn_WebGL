c399bf73eed7264d5caeb92fed94c6ff279320a0
krapnik <krapnik@qq.com>
2021-04-18 17:00:43 +0800

    update

--- WebGL_Programming_Guide/26_LookAtTriangles/main.js
+++ WebGL_Programming_Guide/26_LookAtTriangles/main.js
@@ -2,17 +2,18 @@ const VSHADER_SOURCE = `
     attribute vec4 a_Position;
     attribute vec4 a_Color;
     uniform mat4 u_ViewMatrix;
-    uniform vec4 v_Color;
+    varying vec4 v_Color;
     void main(){
-        gl_Position = u_modelMatrix * a_Position;
+        gl_Position = u_ViewMatrix * a_Position;
+        v_Color = a_Color;
     }
 `;
 
 const FSHADER_SOURCE = `
     precision mediump float;
-    uniform vec4 u_FragColor;
+    varying vec4 v_Color;
     void main(){
-        gl_FragColor = u_FragColor;
+        gl_FragColor = v_Color;
     }
 `;
 
@@ -44,30 +45,38 @@ function main() {
 
     let ponitCount = initVertexBuffers(gl, program);
 
-    let currentAngle = 0.0;
-    let modelMatrix = new Matrix4();
-    let u_modelMatrix = gl.getUniformLocation(program, "u_modelMatrix");
+    let viewMatrix = new Matrix4();
+    let u_ViewMatrix = gl.getUniformLocation(program, "u_ViewMatrix");
 
     //绘制背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
+    viewMatrix.setLookAt(0.2, 0.25, 0.25, 0, 0, 0, 0, 1, 0);
 
-    let tick = () => {
-        currentAngle = animate(currentAngle);
-        draw(gl, ponitCount, currentAngle, modelMatrix, u_modelMatrix);
-        requestAnimationFrame(tick);
-    }
-    tick();
+    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, ponitCount);
 
 }
 
 const initVertexBuffers = (gl, program) => {
     let vertices = new Float32Array([
-        0.0, 0.3,
-        0.3, -0.3,
-        -0.3, -0.3,//gl.TRIANGLES
+        //顶点坐标 (xyz) & 颜色（rgb）
+        0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // 背后绿色
+        -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+        0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
 
+        0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // 中间黄色
+        -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+        0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+        0.0, 0.5, 0.0, 0.4, 0.4, 1.0,  // 前面蓝色
+        -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+        0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
     ]);
+    let FSIZE = vertices.BYTES_PER_ELEMENT;
     let vertexBuffer = gl.createBuffer(); //创建缓冲区对象
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); // 把 WebGLBuffer 对象绑定到指定目标上。
@@ -75,28 +84,15 @@ const initVertexBuffers = (gl, program) => {
 
     //动态设置source
     let a_Position = gl.getAttribLocation(program, "a_Position");
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
     gl.enableVertexAttribArray(a_Position);
 
-    return vertices.length / 2;
+    let a_Color = gl.getAttribLocation(program, "a_Color");
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
+    gl.enableVertexAttribArray(a_Color);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);//解除绑定
+
+    return 9;
 }
 
-const draw = (gl, count, angle, modelMatrix, u_modelMatrix) => {
-    modelMatrix.setRotate(angle, 0, 0, 1);
-
-    gl.uniformMatrix4fv(u_modelMatrix, false, modelMatrix.elements);
-
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.drawArrays(gl.TRIANGLES, 0, count);
-
-}
-
-const animate = (angle) => {
-    let now = new Date();
-    let elapsed = now - g_last;
-    g_last = now;
-    let newAngle = angle + (ANGLE_STEP * elapsed) / 1000;
-    return newAngle %= 360;
-}
