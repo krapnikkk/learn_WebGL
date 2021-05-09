f0cc996fbb9e126c0e40e475e6643c89c2ec51c1
krapnik <krapnik@qq.com>
2021-05-09 12:30:00 +0800

    update folders

---
+++ WebGL_Programming_Guide/01_HelloCanvas/main.js
@@ -0,0 +1,39 @@
+const VSHADER_SOURCE = `
+    void main(){
+        gl_Position = vec4(0.0,0.0,0.0,1.0);
+        gl_PointSize = 10.0;
+    }
+`;
+
+const FSHADER_SOURCE = `
+    void main(){
+        gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    }
+`;
+
+function main() {
+    let canvas = document.querySelector("#webgl");
+    let gl = canvas.getContext("webgl");
+
+    //创建着色器对象
+    let vShader = gl.createShader(gl.VERTEX_SHADER);
+    let fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vShader,VSHADER_SOURCE);
+    gl.shaderSource(fShader,FSHADER_SOURCE);
+    gl.compileShader(vShader);
+    gl.compileShader(fShader);
+
+
+    //创建着色器程序对象
+    let program = gl.createProgram();
+    gl.attachShader(program,vShader);
+    gl.attachShader(program,fShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    //绘制背景色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //清空画布
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
